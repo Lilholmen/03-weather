de2008c83289ecd26df76fb5e3d0f779402882c7
Artur Yandulskii <lilholmen@yandex.ru>
2023-01-29 19:55:08 +0300

    fix secure error for http

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 export const API_KEY = "a3efe8612f5f34abb7b517635167e0ec";
 
 export const API_URL = "https://api.openweathermap.org/data/2.5";
-export const API_URL_GEO = "http://api.openweathermap.org/geo/1.0/direct";
+export const API_URL_GEO = "https://api.openweathermap.org/geo/1.0/direct";
